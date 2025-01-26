8cad5d7ed8395c717222307c36733f041e91c1dc
Lorenzo Morelli <lorenzomorelli95@gmail.com>
2025-01-26 12:35:32 +0100

    - implemented dark and light variable in globals.css - imported images in public - defined primary and secondary color in globals.css - organized folder structure project - implemented title component

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,12 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html
+      lang="en"
+      data-theme="light">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
 }
